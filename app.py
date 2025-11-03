3ec019edf3822d39623a50a4fc108b757cc81e55
Brahmarambha H V <brahmara125@gmail.com>
2025-11-03 19:37:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Flask app folder is ready!")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello! Your Capstone Project Web App is Running Successfully 🎉"
+
+if __name__ == '__main__':
+    app.run(debug=True)
